8f30f81de46b621bb5b7f364a95708cee076bacf
Nate Loum <nate.loum@loumtechnologies.com>
2024-04-18 06:39:29 -0500

    Add basic, working input loop

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <print>
+#include <iostream>
+
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
-    qDebug() << "Hello World";
+    while(true) {
+        std::print("Please enter a command: ");
+        std::string input_line;
+        if (!getline(std::cin, input_line)) {
+            break;
+        }
+
+        std::print("You entered: {0}\n", input_line);
+    }
     return QCoreApplication::exec();
 }
